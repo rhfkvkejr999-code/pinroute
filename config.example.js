a37e5fd36579b5af6ec716f3bfc03ed3a8b63d98
rhfkvkejr999-code <rhfkvkejr999@gmail.com>
2026-05-31 14:34:28 +0900

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -5,7 +5,7 @@
  * 이 파일은 저장소에 커밋해도 안전하도록 예시 값만 포함합니다.
  */
 const CONFIG = {
-  VITE_KAKAO_MAP_KEY: "YOUR_KAKAO_JAVASCRIPT_KEY",
+  VITE_KAKAO_MAP_KEY: "814471ffcb6c88e1be1f34f5442061a2",
   VITE_GEMINI_API_KEY: "YOUR_GEMINI_API_KEY"
 };
 
